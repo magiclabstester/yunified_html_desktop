9ae1281d54be0cb8a7af91a49bbb32195eed0ada
Vu <magiclabstester@gmail.com>
2015-11-24 14:35:44 +0700

    desktop

---
+++ js/mini_slider.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function changeHeight(){
+ var wHeight = $(window).height();
+ var dHeight = $('#page-bg').height();
+ if(dHeight < wHeight)$('#page-bg').height(wHeight-40);
+ else{
+  $('#page-bg').height('auto');
+  var wHeight = $(window).height();
+  var dHeight = $('#page-bg').height();
+  if(dHeight < wHeight)$('#page-bg').height(wHeight-40);
+  
+ }
+};
+ 
+$(document).ready(function () {
+	changeHeight();
+	$(window).resize(function(e) {
+        changeHeight();
+    });
+    $('.sp').first().addClass('active');
+    $('.sp').hide();
+    $('.active').show();
+
+    $('#button-next').click(function () {
+
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':last-child')) {
+            $('.sp').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.sp').fadeOut();
+        $('.active').fadeIn();
+    });
+
+    $('#button-previous').click(function () {
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':first-child')) {
+            $('.sp').last().addClass('active');
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.sp').fadeOut();
+        $('.active').fadeIn();
+    });
+});
